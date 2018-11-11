fd2e9fbf1c84ce5447b0ab4020e16f1ffe6175ec
Stefan <stefan.grisard@tu-dortmund.de>
2018-11-11 20:44:30 +0100

    korrekturen am template

--- .templates/structure/analysis/analysis.py
+++ .templates/structure/analysis/analysis.py
@@ -6,36 +6,31 @@ from uncertainties.unumpy import std_devs as stds
 from uncertainties import correlated_values
 import math
 from scipy.optimize import curve_fit
-import matplotlib.pyplot as plt
 from pint import UnitRegistry
 import latex as l
 r = l.Latexdocument('results.tex')
 u = UnitRegistry()
 Q_ = u.Quantity
-import pandas as pd
-from pandas import Series, DataFrame
-#series = pd.Series(data, index=index)
-# d = pd.DataFrame({'colomn': series})
+import os
 
-#############################################
-#EXAMPLE FOR LATEX TABLE WITH UNP ARRAYS
-a = [1.1, 2.2, 3.3]
-b = [0.1, 0.2, 0.3]
-c = [3, 4, 5]
-d = [6, 7, 8] #EXAMPLE ARRAYS
+def abs_path(filename):
+    return os.path.join(os.path.dirname(__file__), filename)
 
-f = unp.uarray(a, b) #EXAMPLE UARRAY
+import matplotlib as mlp
+mlp.use('pgf')
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
 
-#l.Latexdocument('latex_example.tex').tabular(
-#data = [c, d, f], #Data incl. unp.uarray
-#header = ['\Delta Q / \giga\elementarycharge', 'T_1 / \micro\second', 'T_1 / \micro\second'],
-#places = [1, 1, (1.1, 1.1)],
-#caption = 'testcaption.',
-#label = 'testlabel')
-##############################################
-
-
-
-r.add_result(value = Q_(2, 'meter'), name = 'x')
-r.add_result(value = Q_(ufloat(1.34, 0.02), 'meter / second'), name = 'x_{0}')
-r.makeresults()
+rcParams['font.family'] = 'serif'
+rcParams['font.serif'] = '[lmroman10-regular]:+tlig;'
+rcParams['text.usetex'] = True
+rcParams['pgf.texsystem'] = 'lualatex'
+rcParams['font.size'] = 10
+rcParams['mathtext.fontset'] = 'custom'
+rcParams['pgf.preamble'] = r'\usepackage[locale=DE]{siunitx}'#\usepackage{tgpagella}
+rcParams['text.latex.preamble'] = r'\usepackage[math-style=ISO,bold-style=ISO,sans-style=italic,nabla=upright,partial=upright,]{unicode-math}'
+#rcParams['text.latex.preamble'] = r'\usepackage[locale=DE]{siunitx}'
+#rcParams['text.latex.preamble'] = r'\DeclareSIUnit{\counts}{Counts}'
+rcParams['axes.formatter.use_mathtext'] = True
+rcParams['legend.fontsize'] = 10
+rcParams['savefig.dpi'] = 300
