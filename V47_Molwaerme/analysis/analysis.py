3dfbcc6e54bb4494ac8f4abfbd2fabb6d5619f29
Stefan <stefan.grisard@tu-dortmund.de>
2018-12-10 17:48:51 +0100

    update

--- V47_Molwaerme/analysis/analysis.py
+++ V47_Molwaerme/analysis/analysis.py
@@ -35,8 +35,6 @@ rcParams['font.size'] = 10
 rcParams['mathtext.fontset'] = 'custom'
 rcParams['pgf.preamble'] = r'\usepackage[locale=DE]{siunitx}'#\usepackage{tgpagella}
 rcParams['text.latex.preamble'] = r'\usepackage[math-style=ISO,bold-style=ISO,sans-style=italic,nabla=upright,partial=upright,]{unicode-math}'
-#rcParams['text.latex.preamble'] = r'\usepackage[locale=DE]{siunitx}'
-#rcParams['text.latex.preamble'] = r'\DeclareSIUnit{\counts}{Counts}'
 rcParams['axes.formatter.use_mathtext'] = True
 rcParams['legend.fontsize'] = 10
 rcParams['savefig.dpi'] = 300
@@ -46,15 +44,18 @@ import scipy.constants as const
 #Konstanten und Funktionen
 
 #Molmasse
-M_cu = Q_(63.546 * const.N_A, 'dalton / mol').to('kg / mol')
+M_cu = Q_(63.546 * const.N_A, 'dalton / mol').to('kg / mol') #https://www.webqc.org/molecular-weight-of-Cu.html
 # Probenmasse Versuchsanleitung
 m_probe = Q_(342, 'g')
 
 #Kompressionsmodul
-kappa = Q_(140, 'gigapascal')
+kappa = Q_(14e10, 'pascal') #gross
 
 #Molvolumen
-V_0 = Q_(7.11 / 10**(6), 'm**3 / mol')
+V_0 = Q_(7.0922e-6, 'm**3 / mol') #http://periodictable.com/Elements/029/data.html
+
+#theoriewert
+T_debye_theorie = Q_(343, 'kelvin')
 
 
 #Gaskonstanstante 
@@ -64,7 +65,7 @@ def C_p(U, I, dt, dT):
     return M_cu / m_probe * U * I * dt / dT
 
 def C_v(C_p, alpha, T):
-    return C_p - 9 * alpha**2 * kappa * V_0 * T    
+    return C_p - 9 * alpha**2 * kappa * V_0 * T     
 
 
 #Fits für Ausdehnungskoeffizient und T-R-Charakteristik
@@ -80,7 +81,7 @@ correlated_params = correlated_values(params_alpha, cov_alpha)
 
 params_alpha_units = []
 for i in range(len(correlated_params)):
-    params_alpha_units.append(Q_(correlated_params[i], f'1 / kelvin**{4 - i}'))
+    params_alpha_units.append(Q_(correlated_params[i].n, f'1 / kelvin**{4 - i}'))
     r.add_result(f'param_alpha{i}', params_alpha_units[i])
 
 
@@ -142,27 +143,28 @@ fig.savefig(abs_path('results/alpha_T_R.pdf'), bbox_inches='tight', pad_inches =
 
 # Berechnungen mit den Messdaten 
 R_zyl, U, I, R_prob = np.genfromtxt(abs_path('data/data.txt'), unpack = True)
-R_zyl = Q_(R_zyl, 'kiloohm')
-R_prob = Q_(R_prob, 'kiloohm')
-U = Q_(U, 'volt')
-I = Q_(I, 'milliampere')
-t = np.array([300 * i for i in range(9900 // 300)])
+R_zyl = Q_(unp.uarray(R_zyl, np.repeat(0.0001, len(R_zyl))), 'kiloohm')
+R_prob = Q_(unp.uarray(R_prob, np.repeat(0.0001, len(R_prob))), 'kiloohm')
+U = Q_(unp.uarray(U, np.repeat(0.01, len(U))), 'volt')
+I = Q_(unp.uarray(I, np.repeat(0.1, len(I))), 'milliampere')
+t = Q_(unp.uarray([300 * i for i in range(9900 // 300)], np.repeat(1, 9900 // 300)), 'second')
+
 
 T_zyl = model_T(R_zyl).to('kelvin')
 T_prob = model_T(R_prob).to('kelvin')
 
-mean_diff = np.mean(T_prob - T_zyl)
-r.add_result('mean_T_diff', mean_diff)
+#mean_diff = np.mean(T_prob - T_zyl)
+#r.add_result('mean_T_diff', mean_diff)
 
 fig, ax = plt.subplots(1, 2)
 
-ax[0].plot(t, noms(T_prob), '.', label = r'$T_{\text{Prob}}$')
-ax[0].plot(t, noms(T_zyl), '.', label = r'$T_{\text{Zyl}}$')
+ax[0].plot(noms(t), noms(T_prob), '.', label = r'$T_{\text{Prob}}$')
+ax[0].plot(noms(t), noms(T_zyl), '.', label = r'$T_{\text{Zyl}}$')
 ax[0].set_ylabel('$T / \si{\kelvin}$')
 ax[0].set_xlabel('$t / \si{\second}$')
 ax[0].legend()
 
-ax[1].plot(t, noms(T_prob - T_zyl), '.', color = 'r')
+ax[1].plot(noms(t), noms(T_prob - T_zyl), '.', color = 'r')
 ax[1].set_ylabel(r'$(T_{\text{Prob}} - T_{\text{Zyl}}) / \si{\kelvin}$')
 ax[1].set_xlabel('$t / s$')
 ax[1].set_ylim(-6, 6)
@@ -183,11 +185,15 @@ fig.savefig(abs_path('results/temperaturen.pdf'), bbox_inches='tight', pad_inche
 #Bestimmung der Debye-Temperatur
 
 C_p = C_p(U[:-1], I[:-1], Q_(np.diff(t), 'second'), Q_(np.diff(T_prob), 'kelvin'))
-C_v = C_v(C_p, model_alpha(T_prob[1:]), T_prob[1:]).to('joule / kelvin / mol')
+C_v = C_v(C_p, model_alpha((T_prob[1:] + T_prob[:-1]) / 2), (T_prob[1:] + T_prob[:-1]) / 2).to('joule / kelvin / mol')
+
+print(DataFrame({'C_V': C_v}))
+
+
 
 rcParams['figure.figsize'] = 5.906, 4.5
 fig, ax = plt.subplots(1, 1)
-ax.plot(noms(T_prob[1:]), noms(C_v), 'o', label = 'Daten')
+ax.plot(noms((T_prob[1:] + T_prob[:-1]) / 2), noms(C_v), 'o', label = 'Daten')
 
 
 
@@ -195,16 +201,22 @@ ax.plot(noms(T_prob[1:]), noms(C_v), 'o', label = 'Daten')
 def c_v_debye(T, T_D):
     return [9 * R_gas.magnitude * (T / T_D)**3 * (quad(lambda x: x**4 * np.exp(x) / (np.exp(x) - 1)**2, 0, T_D / T))[0] for T in T]
 
-params, cov = curve_fit(c_v_debye, noms(T_prob[1:]), noms(C_v), p0 = [300])
+params, cov = curve_fit(c_v_debye, noms((T_prob[1:] + T_prob[:-1]) / 2), noms(C_v), p0 = [300])
+print(correlated_values(params, cov))
 T_plot = np.linspace(60, 350, 1000)
 
 ax.set_xlim(T_plot[0], T_plot[-1])
 ax.plot(T_plot, c_v_debye(T_plot, *params), 'r-', label = 'Fit')
 ax.axhline(y = 3 * R_gas.magnitude, color = 'k', linestyle = '--', label = '$3 R$')
-ax.plot(T_plot, c_v_debye(T_plot, 345), 'g-', label = 'Theorie')
-ax.set_xlabel(r'$T / \si{\kelvin}$')
+ax.plot(T_plot, c_v_debye(T_plot, T_debye_theorie.magnitude), 'g-', label = 'Literatur')
+ax.set_xlabel(r'$\overline{T} / \si{\kelvin}$')
 ax.set_ylabel(r'$C_V / \si{\joule  \per \kelvin \per \mol}$')
 ax.legend()
 
 fig.tight_layout()
 fig.savefig(abs_path('results/C_V.pdf'), bbox_inches='tight', pad_inches = 0)
+
+
+# Nochmal mit anderer Methode
+
+
